dfe48902102783b43d08f89a1ff0e69877a5a2fc
Juan Trujillo <jn.trujillo@gmail.com>
2021-06-09 12:26:03 -0400

    primer commit

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middlewares();
+        //rutas de la app
+        this.routes();
+    }
+    
+    middlewares(){
+        //cors
+        this.app.use(cors());
+
+        //parse y lectura del body
+        this.app.use(express.json());
+
+        //directorio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+
+        this.app.use(this.usuariosPath, require('../routes/user'))
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+
+            console.log('Servidor en puerto', this.port);
+            });
+    }
+}
+
+module.exports = Server;
